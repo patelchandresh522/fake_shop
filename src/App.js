a9efe470508965a1b13dc5f2a8a12dfcf1c8b878
patelchandresh522 <patelchandresh522@gmail.com>
2023-06-16 00:32:04 -0400

    update for product display

--- src/App.js
+++ src/App.js
@@ -1,25 +1,25 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Header from './containers/Header';
+import ProductListing from './containers/ProductListing';
+import ProductDetails from './containers/ProductDetails';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div classname="App">
+      <Router>
+      <Header/>
+      <Routes>
+        <Route path = '/' exact element = {<ProductListing/>}/>
+        <Route path = '/product/:productId' exact element = {<ProductDetails/>}/>
+        <Route>404 Not Found!</Route>
+      </Routes>
+      </Router>
+
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
---
+++ src/redux/constants/actionType.js
@@ -0,0 +1,10 @@
+//set products
+//selected product
+// remove selected product
+
+export const ActionTypes = {
+    set_products: 'set_products',
+    selcted_product: 'selected_product',
+    remove_selected_product: 'remove_selected_product'
+
+}
